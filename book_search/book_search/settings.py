727e827b07bb03cb1254fe09733e66879c92fa2b
Prashidha0O1 <rawalprashidha@gmail.com>
2025-07-20 00:04:52 +0545

    updated settings.py

--- book_search/book_search/settings.py
+++ book_search/book_search/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'book_search.urls'
 
 TEMPLATES = [
